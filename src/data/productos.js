fc9ba76469f8ba51b15f60db1f13f757ce7d3c3a
juliietarey-dotcom <juliietarey@gmail.com>
2026-05-15 20:27:09 -0300

    Update productos.js

--- src/data/productos.js
+++ src/data/productos.js
@@ -3,7 +3,7 @@ export const productosData = [
   { categoria: "velas de soja", nombre: "Vela Roma -- 100 cc", precio: "$6000", desc: "Esencia a elcción", img: "/productos/roma.jpg", variantesStock: ["Bamboo", "Papaya", "Pepino-Sandía"] },
   { categoria: "velas de soja", nombre: "Vela Lata -- 100 cc", precio: "$7500", desc: "Esencia a elección", img: "/productos/lata.jpg", variantesStock: ["Limón", "Tilo-Jazmín"] },
   { categoria: "velas de soja", nombre: "Vela Amanecer -- 150 cc", precio: "$8000", desc: "Esencia a elección", img: "/productos/amanecer.jpg", variantesStock: ["Vainilla", "Limón", "Papaya"] },
-  { categoria: "velas de soja", nombre: "Vela Hexagonal -- 150 cc", precio: "$8000", desc: "Esencia a elección", img: "/productos/hexagonal.jpg", variantesStock: ["Vainilla", "Limón", "Tilo-Jazmín", "Papaya"] },
+  { categoria: "velas de soja", nombre: "Vela Hexagonal -- 150 cc", precio: "$8000", desc: "Esencia a elección", img: "/productos/hexagonal.jpg", variantesStock: ["Limón", "Tilo-Jazmín", "Papaya"] },
   { categoria: "velas de soja", nombre: "Vela Olivia -- 180 cc", precio: "$9500", desc: "Esencia a elección", img: "/productos/olivia.jpg", variantesStock: ["Coco", "Vainilla"] },
   { categoria: "velas de soja", nombre: "Vela Cuenco de madera -- 100 cc", precio: "$10000", desc: "Esencia a elección", img: "/productos/cuenco.jpg", variantesStock: ["Coco", "Bamboo"] },
   { categoria: "velas de soja", nombre: "Vela Caramelera -- 80 cc", precio: "$11000", desc: "Esencia a elección", img: "/productos/caramelera.jpg", variantesStock: [] },
